364955c447fa86c3e73351fcbf4dce00471e9d28
alexey.radkovich <alexey.radkovich@gmail.com>
2017-10-06 15:28:17 +0300

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
@@ -6,6 +9,14 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    console.log(Data);
 
     return element;
 }

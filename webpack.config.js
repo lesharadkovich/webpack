2d033991196a8b5098a074a953168fbdc4d3f8ab
alexey.radkovich <alexey.radkovich@gmail.com>
2017-10-09 16:36:15 +0300

    Authoring Libraries

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,19 @@
-const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+var path = require('path');
 
 module.exports = {
-    entry: {
-        main: './src/index.js',
-        vendor: [
-            'lodash'
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HTMLWebpackPlugin({
-            title: 'Caching'
-        }),
-        new webpack.HashedModuleIdsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'runtime'
-        })
-    ],
+    entry: './src/index.js',
     output: {
-        filename: '[name].[chunkhash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'webpack-numbers.js',
+        library: 'webpackNumbers',
+        libraryTarget: 'umd'
+    },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
 };
